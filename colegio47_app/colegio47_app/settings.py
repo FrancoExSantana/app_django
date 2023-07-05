2b087f6988916bef1972c99626794463c7f32103
AgustinEscalante23 <131416066+AgustinEscalante23@users.noreply.github.com>
2023-07-05 18:06:58 -0300

    junco te banco

--- colegio47_app/colegio47_app/settings.py
+++ colegio47_app/colegio47_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rqc@_z_=$tf7g-n#6rsje%0skuhcxqzeiy*wo64+uvkn&wspap'
+SECRET_KEY = 'django-insecure--_rk3jl(&yo3*ee1st$g7_k2bfz#!=wefarf8#=@a-zhn)bovv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
