b0e31176d2e4a522fae35e5f6b507cea6f2a99b8
FrancoExSantana <FrancoExSantana94@gmail.com>
2023-07-09 17:27:38 -0300

    Ahora de veritas registro

--- colegio47_app/registros/admin.py
+++ colegio47_app/registros/admin.py
@@ -1,9 +1,4 @@
-<<<<<<< Updated upstream
+
 from django.contrib import admin
 
 # Register your models here.
-=======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> Stashed changes
