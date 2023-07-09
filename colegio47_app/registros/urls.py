3730a3cb7d9f6fa8016a8a942d5bc595520d90f2
FrancoExSantana <FrancoExSantana94@gmail.com>
2023-07-09 17:12:25 -0300

    Registros Terminado
    Se han añadido todas los templetes y urls para un registro de usuarios satisfactorio

--- colegio47_app/registros/urls.py
+++ colegio47_app/registros/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name= 'home'),
     path('signup/', views.signup, name= 'signup'),
+    path('homepage/', views.homepage, name='homepage'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
 ]
--- colegio47_app/registros/views.py
+++ colegio47_app/registros/views.py
@@ -1,32 +1,63 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
+
 
 def signup(request):
 
     if request.method == 'GET':
         return render(request, 'signup.html', {
-            'form': UserCreationForm 
+            'form': UserCreationForm
         })
     else:
-        if request.POST['password1'] == request.POST    ['password2']:
+        if request.POST['password1'] == request.POST['password2']:
             try:
-             user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
-             user.save()
-             return HttpResponse('Los datos se han registrado con exito')
-            except:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('homepage')
+            except IntegrityError:
                 return render(request, 'signup.html', {
                     'form': UserCreationForm,
-                    'error': 'El usuario ingresado ya existe'
-                }),
+                    'error': 'El usuario creado ya existe'
+                })
+
         return render(request, 'signup.html', {
             'form': UserCreationForm,
             'error': 'Las contraseñas no coinciden'
         })
 
 
-   
+def homepage(request):
+    return render(request, 'homepage.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'sigin.html', {
+                'form': AuthenticationForm,
+                'error': 'El usuario o la contraseña son incorrectos'
+            })
+        else: 
+            login(request, user)
+            return redirect('homepage')
