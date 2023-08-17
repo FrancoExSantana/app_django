21b5880d7ebda6856fc8da21192bc5ce6ee79f10
agustinesc200@gmail.com <agustinesc200@gmail.com>
2023-08-16 21:58:39 -0300

    .

--- colegio47_app/mi_app/views.py
+++ colegio47_app/mi_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("index page")
+def hello(request, username):
+    return HttpResponse("<h2>hello %s</h2>" % username)
+
+def about(request):
+    return HttpResponse('About')
+
+
+
+                   
+    
