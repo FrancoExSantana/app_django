d859e962a621a2b3e47cbb47609fd2ea970f0726
FrancoExSantana <FrancoExSantana94@gmail.com>
2023-08-18 12:20:26 -0300

    Cambios de diseño al homepage

--- colegio47_app/colegio47_app/urls.py
+++ colegio47_app/colegio47_app/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('registros.urls')),
+    path('', include('mi_app.urls')),
 ]
--- colegio47_app/mi_app/urls.py
+++ colegio47_app/mi_app/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
-    path('', views.index, name= 'home'),
-    path('homepage/', views, name= 'clases'),
+    path('homepage/', views.homepage, name= 'inicio'),
     path('about/', views.about, name='inscripciones'),
-    path('carreras/', views.index, name='carreras'),
     ]
-    
